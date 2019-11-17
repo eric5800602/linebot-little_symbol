a5341848c6ce2c002f505d81d06d79f952320b5b
eric5800602 <eric5800602@gmail.com>
2019-11-17 23:52:58 +0800

    make it better

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ if channel_access_token is None:
 
 line_bot_api = LineBotApi(channel_access_token)
 parser = WebhookParser(channel_secret)
-
+ 
 
 @app.route("/callback", methods=["POST"])
 def callback():
