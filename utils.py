fb44444714dc1420a86269cb2f98ddb5aa796dcb
eric5800602 <eric5800602@gmail.com>
2019-11-21 00:52:50 +0800

    fixed image transfering error

--- utils.py
+++ utils.py
@@ -16,13 +16,7 @@ def send_text_message(reply_token, text):
 
 def send_image_url(reply_token, img_url):
     line_bot_api = LineBotApi(channel_access_token)
-    line_bot_api.reply_message(reply_token, 
-    {
-        type: 'image',
-        originalContentUrl: 'https://developers.line.biz/media/messaging-api/messages/image-full-04fbba55.png',
-        previewImageUrl: 'https://developers.line.biz/media/messaging-api/messages/image-167efb33.png'
-    }
-    )
+    line_bot_api.reply_message(reply_token,ImageSendMessage(original_content_url='https://developers.line.biz/media/messaging-api/messages/image-full-04fbba55.png', preview_image_url='https://developers.line.biz/media/messaging-api/messages/image-167efb33.png'))
     return "OK"
 """
 def send_button_message(id, text, buttons):
