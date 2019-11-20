a5068f24530e0cd1724bc2047c4688da90567298
eric5800602 <eric5800602@gmail.com>
2019-11-21 02:10:15 +0800

    modify meme image url

--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ import random
 
 from linebot import LineBotApi, WebhookParser
 from linebot.models import *
+from bs4 import BeautifulSoup
+import requests
 
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
 
@@ -16,7 +18,10 @@ def send_text_message(reply_token, text):
 
 def send_image_url(reply_token, img_url):
     line_bot_api = LineBotApi(channel_access_token)
-    line_bot_api.reply_message(reply_token,ImageSendMessage(original_content_url='https://memes.tw/image/'+str(random.randint(1,5000)), preview_image_url='https://memes.tw/image/'+str(random.randint(1,5000))))
+    head_Html = 'https://memes.tw/image/'+str(random.randint(1,5000))
+    res = requests.get(head_Html, timeout=30)
+    soup = BeautifulSoup(res.text,'lxml')
+    line_bot_api.reply_message(reply_token,ImageSendMessage(original_content_url='https://memes.tw'+str(soup.img['src']), preview_image_url='https://memes.tw'+str(soup.img['src'])))
     
     return "OK"
 """
