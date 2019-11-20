71da17115327cccbb39b057099c9312df73b582c
eric5800602 <eric5800602@gmail.com>
2019-11-21 01:16:45 +0800

    modify meme image url

--- utils.py
+++ utils.py
@@ -16,7 +16,8 @@ def send_text_message(reply_token, text):
 
 def send_image_url(reply_token, img_url):
     line_bot_api = LineBotApi(channel_access_token)
-    line_bot_api.reply_message(reply_token,ImageSendMessage(original_content_url='https://memes.tw/image/'+str(random.randint(1,5000)), preview_image_url='https://memes.tw/image/'+str(random.randint(1,5000)))
+    line_bot_api.reply_message(reply_token,ImageSendMessage(original_content_url='https://memes.tw/meme/95c8f82420a33b3f292641977a15af8e.png', preview_image_url='https://memes.tw/meme/95c8f82420a33b3f292641977a15af8e.png')
+
     return "OK"
 """
 def send_button_message(id, text, buttons):
