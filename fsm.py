c25a8d78087ed40b58ebed24227870913ac87700
eric5800602 <eric5800602@gmail.com>
2019-11-21 00:43:14 +0800

    fixed image transfering error

--- fsm.py
+++ fsm.py
@@ -1,7 +1,7 @@
 from transitions.extensions import GraphMachine
 
 from utils import send_text_message
-
+from utils import send_image_url
 
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
